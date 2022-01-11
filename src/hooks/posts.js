8a723d6ce7fb9bc05342a737d66b789f240d5b42
José Enrique <otipip90@gmail.com>
2022-01-10 23:23:41 -0500

    save changes in posts

--- src/hooks/posts.js
+++ src/hooks/posts.js
@@ -1,11 +1,12 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { createNewPost, getPosts } from '../api/posts';
 
+const KEY = 'posts';
 export function useMutatePost(){
     const queryClient = useQueryClient();
     const {mutate,isLoading, error, isSuccess,reset} = useMutation(createNewPost,{
         onSuccess: ()=>{
-          queryClient.invalidateQueries(['posts']);//cache invalidation action, if cache is invalid then reactQuery refresh the posts list.
+          queryClient.invalidateQueries([KEY]);//cache invalidation action, if cache is invalid then reactQuery refresh the posts list.
         }
       });
       return {mutate,isLoading, error, isSuccess,reset}
@@ -14,7 +15,7 @@ export function useMutatePost(){
 export function usePosts(){
     const qc =  useQueryClient()
     //Queries
-    const {data: posts,error, isLoading, isFetching/*, isIdle, refetch*/} = useQuery(['posts'],getPosts,{
+    const {data: posts,error, isLoading, isFetching/*, isIdle, refetch*/} = useQuery([KEY],getPosts,{
       //refetchOnWindowFocus:false, //this property refresh the data example if we move for other tabs and return react-query refresh our data.
       //staleTime: <time>|infinity// exist other properties to handle this kind of situation.
       //enabled:false, //this property is for no load the data automatically
