80f7326ff6e6230ca5c55424f39889f8a7a58ab9
José Enrique <otipip90@gmail.com>
2022-01-12 00:40:28 -0500

    created pagination with react query

--- src/services/GitHubServices.js
+++ src/services/GitHubServices.js
@@ -1,6 +1,7 @@
-export async function getGitHubData({ page = 1 }) {
+export async function getGitHubData(page = 1) {
+  debugger;
   const response = await fetch(
-    `https://api.github.com/search/repositories?q=topic:reactjs&per_page=30&page=${page}`
+    `https://api.github.com/search/repositories?q=topic:reactjs&per_page=3&page=${page}`
   );
   return response.json();
 }
